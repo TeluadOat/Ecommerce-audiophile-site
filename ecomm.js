17d4d163c017e7730ddb696df5deb1c4ec50964c
Olubodun Adeola <olubodunadeola2000@gmail.com>
2025-06-28 02:05:55 +0100

    worked on the header

--- ecomm.js
+++ ecomm.js
@@ -1,11 +1,30 @@
 const paras = document.querySelectorAll('.nav-dir p');
 const dropd = document.querySelectorAll('.dropd');
 const arrow = document.querySelectorAll('.arrow');
-
+const menuToggle = document.getElementById('menu-toggle');
+const menuDropdown = document.getElementById('menu-dropdown');
 
 for (let i = 0; i < paras.length; i++) {
-    paras[i].addEventListener("click", function () {
-        dropd[i].classList.toggle("show");
-        arrow[i].classList.toggle("arrow-rotate")
+    paras[i].addEventListener("click", function (e) {
+        dropd[i].classList.toggle("block-show");
+        arrow[i].classList.toggle("arrow-rotate");
+        e.stopPropagation();
+    })
+    document.addEventListener("click", (e) => {
+        if (!paras[i].contains(e.target) && !dropd[i].contains(e.target)) {
+            dropd[i].classList.remove("block-show");
+            arrow[i].classList.remove("arrow-rotate");
+        }
     })
 }
+
+menuToggle.addEventListener("click", function (e) {
+    menuDropdown.classList.toggle("collapsed");
+    e.stopPropagation();
+})
+
+document.addEventListener("click", (e) => {
+    if (!menuToggle.contains(e.target) && !menuDropdown.contains(e.target)) {
+        menuDropdown.classList.add("collapsed")
+    }
+})
